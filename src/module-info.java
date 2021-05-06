9a7ae994ae29f4363a8cdb4bd668ac72b4b50785
Max Kruggel <support@redigermany.de>
2021-05-06 20:44:39 +0200

    Imported web, exported mvc

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,11 @@
 module ReDiNet {
     exports com.github.redigermany.redinet;
+    exports com.github.redigermany.redinet.view;
+    exports com.github.redigermany.redinet.controller;
     requires javafx.base;
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.web;
+    opens com.github.redigermany.redinet.controller;
 }
