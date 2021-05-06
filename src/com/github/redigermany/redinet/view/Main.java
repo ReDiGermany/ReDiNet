cdcf0f7e07a9c247fd4a4add3ff33faaa34f44f2
Max Kruggel <support@redigermany.de>
2021-05-06 20:46:30 +0200

    Added Main to load program

---
+++ src/com/github/redigermany/redinet/view/Main.java
@@ -0,0 +1,16 @@
+package com.github.redigermany.redinet.view;
+
+import com.github.redigermany.redinet.controller.MainLayout;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        javafx.application.Application.launch(MainLayout.class);
+//        new Thread() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }.start();
+    }
+}
