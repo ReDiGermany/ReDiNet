2ad234fdbdadd051c063bd611766589c643e2214
Max Kruggel <support@redigermany.de>
2021-06-05 19:17:29 +0200

    Removed logging Added getall/clear all for history

--- src/com/github/redigermany/redinet/controller/HistoryController.java
+++ src/com/github/redigermany/redinet/controller/HistoryController.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class HistoryController {
@@ -15,11 +16,26 @@ public class HistoryController {
         return INSTANCE;
     }
     private final String fileName = "history.log";
-    private final Logger logger = new Logger(Logger.TYPES.ALL);
+    private final Logger logger = new Logger(Logger.TYPES.NONE);
     private ArrayList<String> history = new ArrayList<>();
+
+    public ArrayList<String> getList(){
+        return history;
+    }
+
+    public void clear(){
+        history.clear();
+        try(FileWriter fw = new FileWriter(fileName,false)){
+            fw.append("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private HistoryController(){
         readConfig();
     }
+
     private void readConfig(){
         try(BufferedReader fr = new BufferedReader(new FileReader(fileName))){
             String line;
@@ -30,6 +46,7 @@ public class HistoryController {
             logger.error(e.getMessage());
         }
     }
+
     public void addUrl(String url){
         history.add(url);
         try(FileWriter fw = new FileWriter(fileName,true)){
@@ -38,6 +55,7 @@ public class HistoryController {
             e.printStackTrace();
         }
     }
+
     public ArrayList<String> search(String text){
         ArrayList<String> ret = new ArrayList<>();
         for(String line:history){
