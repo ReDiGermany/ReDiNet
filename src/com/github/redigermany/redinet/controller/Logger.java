272d614f9b5ffb15be7cdfb7bc10fa30f9ba2fdb
Max Kruggel <support@redigermany.de>
2021-06-17 00:05:35 +0200

    Added java doc

--- src/com/github/redigermany/redinet/controller/Logger.java
+++ src/com/github/redigermany/redinet/controller/Logger.java
@@ -1,9 +1,13 @@
 package com.github.redigermany.redinet.controller;
 
 import java.time.LocalDateTime;
-import java.util.Locale;
 
+/**
+ * Permission based logger to keep clear console
+ * @author Max Kruggel
+ */
 public class Logger {
+
     public enum TYPES {
         ALL,
         DEBUG,
@@ -11,13 +15,58 @@ public class Logger {
         ERROR,
         NONE
     }
-    private TYPES type;
+    private final TYPES type;
     private final String name;
     private String id;
-    public Logger setId(String id){
+
+    /**
+     * Sets the prefix id
+     * @param id
+     */
+    public void setId(String id){
         this.id = id;
-        return this;
     }
+
+    /**
+     * Checks the type and logs if allowed to console width template >[time,date] [type name] [file name] {[id]} text<
+     * @param type the desired permission type
+     * @param text the text to print
+     */
+    private void log(TYPES type,String text){
+        if(type.ordinal() >= this.type.ordinal()){
+            LocalDateTime now = LocalDateTime.now();
+            System.out.printf("[%s %s] [%s] [%s]%s %s%n",now.toLocalTime(),now.toLocalDate(),type.name().toUpperCase(),name,(this.id!=null?" ["+id+"]":""),text);
+        }
+    }
+
+    /**
+     * Logs with debug permission
+     * @param text the text to print
+     */
+    public void debug(String text){
+        log(TYPES.DEBUG,text);
+    }
+
+    /**
+     * Logs with error permission
+     * @param text the text to print
+     */
+    public void error(String text){
+        log(TYPES.ERROR,text);
+    }
+
+    /**
+     * Logs with info permission
+     * @param text the text to print
+     */
+    public void info(String text){
+        log(TYPES.INFO,text);
+    }
+
+    /**
+     * Initializes the logger with desired permission type
+     * @param type the desired permission
+     */
     public Logger(TYPES type){
         try {
             throw new Exception();
@@ -27,19 +76,5 @@ public class Logger {
 //            System.out.println("Logger active for "+name+" @"+type.name());
         }
     }
-    private void log(TYPES type,String text){
-        if(type.ordinal() >= this.type.ordinal()){
-            LocalDateTime now = LocalDateTime.now();
-            System.out.printf("[%s %s] [%s] [%s]%s %s%n",now.toLocalTime(),now.toLocalDate(),type.name().toUpperCase(),name,(this.id!=null?" ["+id+"]":""),text);
-        }
-    }
-    public void debug(String text){
-        log(TYPES.DEBUG,text);
-    }
-    public void error(String text){
-        log(TYPES.ERROR,text);
-    }
-    public void info(String text){
-        log(TYPES.INFO,text);
-    }
+
 }
